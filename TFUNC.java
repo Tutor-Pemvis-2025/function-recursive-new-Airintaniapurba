c898be9b23bcf29c8bc2c2c58c45873c3b80a288
airintania19-commits <airintania19@gmail.com>
2025-11-11 20:57:02 +0700

    airin
    selesai

---
+++ TFUNC.java
@@ -0,0 +1,29 @@
+//12S25046 - Airin Tania Purba
+
+import java.util.*;
+import java.lang.Math;
+
+public class TFUNC {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n, hasil;
+
+        n = Integer.parseInt(input.nextLine());
+        System.out.println("Bilangan yang dimasukkan (N):" + n);
+        System.out.println("Hasil perhitungan rekursif:" + "(" + n + ")" + ":" + angka(n));
+    }
+    
+    public static int angka(int n) {
+        int result;
+
+        if (n == 0) {
+            result = 0;
+        } else {
+            System.out.println(Integer.toString(n) + "+");
+            result = n + angka(n - 1);
+        }
+        
+        return result;
+    }
+}
